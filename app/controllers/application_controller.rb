90fad2ede5ecff5a45e58e08bf63d95853fec7fa
Kota-nakagawa0629 <ayuri20130629@hotmail.com>
2021-03-13 05:02:14 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
 
-    private
+include SessionsHelper
+
+  private
 
   def require_user_logged_in
     unless logged_in?
