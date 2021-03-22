d4320c1310af9b525d20f21b2dad3784c8579b72
Kota-nakagawa0629 <ayuri20130629@hotmail.com>
2021-03-22 10:30:51 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+  # before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   def index
-    if logged_in?
+    # if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+    # end
   end
 
   def show
